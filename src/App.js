264c0acd355f15e0c2ddbc2ba0de68723736350f
ParthGupta-28 <pg6272695@gmail.com>
2023-02-10 20:51:40 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  IconButton,
+  Link,
+  VStack,
+} from "@chakra-ui/react";
+import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { GrMail } from "react-icons/gr";
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <motion.div
+      animate={{ opacity: 1, y: "10%" }}
+      transition={{ delay: 0.3, duration: 0.8 }}
+      initial={{ opacity: 0 }}
+    >
+      <VStack>
+        <Flex justifyContent={"center"} alignItems={"center"} mt="13%">
+          <Heading
+            sx={{
+              fontSize: "6rem",
+            }}
+          >
+            Parth Gupta
+          </Heading>
+        </Flex>
+        <HStack
+          className="notshow"
+          position="initial"
+          spacing="2rem"
+          height="5rem"
+          width={"100%"}
+          justifyContent={"center"}
+          color="white"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Box boxSize="5rem" justifyContent={"center"}>
+            <Link href="https://twitter.com/ParthGupta2002" isExternal>
+              <IconButton
+                icon={<BsTwitter />}
+                isRound="true"
+                p={"revert"}
+                sx={{ fontSize: "3rem", boxSize: "4rem" }}
+                variant="outline"
+                borderColor={"white"}
+                _hover={{ boxSize: "4.5rem", margin: "-1px" }}
+                color="white"
+              ></IconButton>
+            </Link>
+          </Box>
+
+          <Box boxSize="5rem" justifyContent={"center"}>
+            <Link href="https://github.com/ParthGupta-28" isExternal>
+              <IconButton
+                icon={<BsGithub />}
+                isRound="true"
+                p={"revert"}
+                sx={{
+                  fontSize: "3rem",
+                  boxSize: "4rem",
+                }}
+                variant="outline"
+                borderColor={"white"}
+                _hover={{ boxSize: "4.5rem", margin: "-1px" }}
+                color="white"
+              ></IconButton>
+            </Link>
+          </Box>
+
+          <Box boxSize="5rem" justifyContent={"center"}>
+            <Link href="https://www.linkedin.com/in/parthgupta2809" isExternal>
+              <IconButton
+                icon={<BsLinkedin />}
+                isRound="true"
+                p={"revert"}
+                sx={{ fontSize: "2.5rem", boxSize: "4rem" }}
+                variant="outline"
+                borderColor={"white"}
+                _hover={{ boxSize: "4.5rem", margin: "-1px" }}
+                color="white"
+              ></IconButton>
+            </Link>
+          </Box>
+
+          <Box boxSize="5rem" justifyContent={"center"}>
+            <Link href="mailto: pg6272695@gmail.com" isExternal>
+              <IconButton
+                icon={<GrMail />}
+                isRound="true"
+                p={"revert"}
+                sx={{ fontSize: "3rem", boxSize: "4rem" }}
+                variant="outline"
+                borderColor={"white"}
+                _hover={{ boxSize: "4.5rem", margin: "-1px" }}
+                color="white"
+              ></IconButton>
+            </Link>
+          </Box>
+        </HStack>
+      </VStack>
+    </motion.div>
   );
 }
 
