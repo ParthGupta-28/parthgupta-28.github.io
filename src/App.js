2ca8adcbdc22a039633c84b9290e20034b490d18
ParthGupta-28 <pg6272695@gmail.com>
2023-02-13 21:49:08 +0530

    final try

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import {
+  Center,
   Flex,
   Heading,
   HStack,
@@ -18,101 +19,106 @@ function App() {
       transition={{ delay: 0.3, duration: 0.8 }}
       initial={{ opacity: 0 }}
     >
-      <VStack maxW={{ base: "90vw", sm: "80vw", lg: "50vw" }}>
-        <Flex justifyContent={"center"} alignItems={"center"} mt="13%">
-          <Heading
-            sx={{
-              fontSize: "6rem",
-            }}
+      <Center>
+        <VStack maxW={{ base: "90vw", sm: "80vw", lg: "50vw" }} mt={"15%"}>
+          <Flex justifyContent={"center"} alignItems={"center"}>
+            <Heading
+              sx={{
+                fontSize: "6rem",
+              }}
+            >
+              Parth Gupta
+            </Heading>
+          </Flex>
+          <HStack
+            className="notshow"
+            position="initial"
+            spacing="2.5rem"
+            height="5rem"
+            width={"100%"}
+            justifyContent={"center"}
+            color="white"
           >
-            Parth Gupta
-          </Heading>
-        </Flex>
-        <HStack
-          className="notshow"
-          position="initial"
-          spacing="2.5rem"
-          height="5rem"
-          width={"100%"}
-          justifyContent={"center"}
-          color="white"
-        >
-          <motion.div
-            whileHover={{ scale: 1.3 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Link href="https://twitter.com/ParthGupta2002" isExternal>
-              <IconButton
-                icon={<BsTwitter />}
-                isRound="true"
-                p={"revert"}
-                sx={{ fontSize: "3rem", boxSize: "4rem" }}
-                variant="outline"
-                borderColor={"white"}
-                color="white"
-                _hover={{}}
-              ></IconButton>
-            </Link>
-          </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.3 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Link href="https://twitter.com/ParthGupta2002" isExternal>
+                <IconButton
+                  icon={<BsTwitter />}
+                  isRound="true"
+                  p={"revert"}
+                  sx={{ fontSize: "3rem", boxSize: "4rem" }}
+                  variant="outline"
+                  borderColor={"white"}
+                  color="white"
+                  _hover={{}}
+                ></IconButton>
+              </Link>
+            </motion.div>
 
-          <motion.div
-            whileHover={{ scale: 1.3 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Link href="https://github.com/ParthGupta-28" isExternal>
-              <IconButton
-                icon={<BsGithub />}
-                isRound="true"
-                p={"revert"}
-                sx={{
-                  fontSize: "3rem",
-                  boxSize: "4rem",
-                }}
-                variant="outline"
-                borderColor={"white"}
-                color="white"
-                _hover={{}}
-              ></IconButton>
-            </Link>
-          </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.3 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Link href="https://github.com/ParthGupta-28" isExternal>
+                <IconButton
+                  icon={<BsGithub />}
+                  isRound="true"
+                  p={"revert"}
+                  sx={{
+                    fontSize: "3rem",
+                    boxSize: "4rem",
+                  }}
+                  variant="outline"
+                  borderColor={"white"}
+                  color="white"
+                  _hover={{}}
+                ></IconButton>
+              </Link>
+            </motion.div>
 
-          <motion.div
-            whileHover={{ scale: 1.3 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Link href="https://www.linkedin.com/in/parthgupta2809" isExternal>
-              <IconButton
-                icon={<BsLinkedin />}
-                isRound="true"
-                p={"revert"}
-                sx={{ fontSize: "2.5rem", boxSize: "4rem" }}
-                variant="outline"
-                borderColor={"white"}
-                color="white"
-                _hover={{}}
-              ></IconButton>
-            </Link>
-          </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.3 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Link
+                href="https://www.linkedin.com/in/parthgupta2809"
+                isExternal
+              >
+                <IconButton
+                  icon={<BsLinkedin />}
+                  isRound="true"
+                  p={"revert"}
+                  sx={{ fontSize: "2.5rem", boxSize: "4rem" }}
+                  variant="outline"
+                  borderColor={"white"}
+                  color="white"
+                  _hover={{}}
+                ></IconButton>
+              </Link>
+            </motion.div>
 
-          <motion.div
-            whileHover={{ scale: 1.3 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Link href="mailto: pg6272695@gmail.com" isExternal>
-              <IconButton
-                icon={<GrMail />}
-                isRound="true"
-                p={"revert"}
-                sx={{ fontSize: "3rem", boxSize: "4rem" }}
-                variant="outline"
-                borderColor={"white"}
-                color="white"
-                _hover={{}}
-              ></IconButton>
-            </Link>
-          </motion.div>
-        </HStack>
-      </VStack>
+            <motion.div
+              whileHover={{ scale: 1.3 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Link href="mailto: pg6272695@gmail.com" isExternal>
+                <IconButton
+                  icon={<GrMail />}
+                  isRound="true"
+                  p={"revert"}
+                  sx={{ fontSize: "3rem", boxSize: "4rem" }}
+                  variant="outline"
+                  borderColor={"white"}
+                  color="white"
+                  _hover={{}}
+                ></IconButton>
+              </Link>
+            </motion.div>
+          </HStack>
+        </VStack>
+      </Center>
     </motion.div>
   );
 }
